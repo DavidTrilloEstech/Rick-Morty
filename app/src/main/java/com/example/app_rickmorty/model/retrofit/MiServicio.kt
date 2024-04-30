8d878c7d8f7ed6e2de11d905bb50f8e0b3c98224
MariaSanchez <maria.estech@escuelaestech.es>
2024-04-30 09:11:45 +0200

    prueba nuevo mac

--- app/src/main/java/com/example/app_rickmorty/model/retrofit/MiServicio.kt
+++ app/src/main/java/com/example/app_rickmorty/model/retrofit/MiServicio.kt
@@ -1,5 +1,6 @@
 package com.example.app_rickmorty.model.retrofit
 
+import com.example.app_rickmorty.model.data.Localizacion.Localizacion
 import com.example.app_rickmorty.model.data.personajes.Character
 import retrofit2.Response
 import retrofit2.http.GET
@@ -7,4 +8,7 @@ import retrofit2.http.GET
 interface MiServicio {
     @GET("character")
     suspend fun obtenerPersonajes(): Response<Character>
+
+    @GET("location")
+    suspend fun obtenerLocalizacion(): Response<Localizacion>
 }
