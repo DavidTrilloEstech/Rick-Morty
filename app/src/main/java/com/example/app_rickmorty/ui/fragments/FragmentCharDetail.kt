32cdb8ee3da391234fa34b72a4dd6bb0f60b02e7
DavidTrilloEstech <maria.estech@escuelaestech.es>
2024-05-23 14:25:26 +0200

    falta el layout episode

--- app/src/main/java/com/example/app_rickmorty/ui/fragments/FragmentCharDetail.kt
+++ app/src/main/java/com/example/app_rickmorty/ui/fragments/FragmentCharDetail.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -81,6 +82,7 @@ class FragmentCharDetail : Fragment() {
         binding.recyclerView2.adapter=EpisodeAdapter(character.episode, object  : EpisodeAdapter.Myclick{
             override fun onHolderClick(episode: String) {
                 viewModel.getEpisode(episode.toInt())
+                findNavController().navigate(R.id.action_fragmentCharDetail_to_fragmentEpisode)
             }
 
         })
